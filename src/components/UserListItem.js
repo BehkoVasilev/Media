eb312769a990cc925c090f955f9b9304099be7d9
BehkoVasilev <vencislavvsilev@gmail.com>
2023-05-23 20:21:58 +0300

    add delete button
     display error message if it has

--- src/components/UserListItem.js
+++ src/components/UserListItem.js
@@ -1,10 +1,25 @@
+import { GoTrashcan } from "react-icons/go";
+import useThunk from "../hooks/useThunk";
+import { removeUser } from "../store";
+import Button from "./Button";
+
 function UserListItem({ user }) {
+    const [doRemoveUser, isLoading, error] = useThunk(removeUser);
+
+    const handleClick = () =>{
+        doRemoveUser(user)
+    }
+
     return (
         <div className="mb-2 border rounded">
-                <div className="flex p-2 fustify-between items-center cursor-pointer">
-                    {user.name}
-                </div>
+            <div className="flex p-2 fustify-between items-center cursor-pointer">
+                <Button loading={isLoading} onClick={handleClick}>
+                    <GoTrashcan />
+                </Button>
+                {error && <div>Error deleting user.</div>}
+                {user.name}
             </div>
+        </div>
     )
 };
 
