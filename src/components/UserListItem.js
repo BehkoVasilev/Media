f81f38ae9b3deef8d9cb0e36e91582359ad37d51
BehkoVasilev <vencislavvsilev@gmail.com>
2023-05-23 21:01:07 +0300

    add some styling

--- src/components/UserListItem.js
+++ src/components/UserListItem.js
@@ -6,18 +6,20 @@ import Button from "./Button";
 function UserListItem({ user }) {
     const [doRemoveUser, isLoading, error] = useThunk(removeUser);
 
-    const handleClick = () =>{
+    const handleClick = () => {
         doRemoveUser(user)
     }
 
     return (
         <div className="mb-2 border rounded">
             <div className="flex p-2 fustify-between items-center cursor-pointer">
-                <Button loading={isLoading} onClick={handleClick}>
-                    <GoTrashcan />
-                </Button>
-                {error && <div>Error deleting user.</div>}
-                {user.name}
+                <div className="flex flex-row items-center justify-between">
+                    <Button className="mr-3" loading={isLoading} onClick={handleClick}>
+                        <GoTrashcan />
+                    </Button>
+                    {error && <div>Error deleting user.</div>}
+                    {user.name}
+                </div>
             </div>
         </div>
     )
