89ac78ca691e10a7b90c656ae3b7f13cd5f95b92
BehkoVasilev <vencislavvsilev@gmail.com>
2023-06-19 10:16:24 +0300

    delete album

--- src/components/AlbumsListItem.js
+++ src/components/AlbumsListItem.js
@@ -1,10 +1,17 @@
 import { GoTrashcan } from "react-icons/go";
+import { useRemoveAlbumMutation } from "../store";
 import Button from "./Button";
 import ExpandablePanel from "./ExpandablePanel";
 
 function AlbumsListItem({ album }) {
+    const [removeAlbum, result] = useRemoveAlbumMutation();
+
+    const handleRemoveClick = () => {
+        removeAlbum(album);
+    }
+
     const header = <div className="flex flex-row items-center justify-between">
-        <Button>
+        <Button loading={result.isLoading} onClick={handleRemoveClick}>
             <GoTrashcan />
         </Button>
         {album.title}
