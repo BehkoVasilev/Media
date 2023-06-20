a742d01de21e59174951c7ace77a5e4b0e54e5e8
BehkoVasilev <vencislavvsilev@gmail.com>
2023-06-20 07:31:37 +0300

    Create photoApi.js

---
+++ src/store/apis/photosApi.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query";
+
+const photosApi = createApi({
+    reducerPath: 'photos',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'http://localhost:3005'
+    }),
+    endpoints(builder) {
+        return {
+            fetchPhotos: builder.query({
+
+            }),
+            addPhoto: builder.mutation({
+
+            }),
+            removePhoto: builder.mutation({
+
+            })
+        };
+    },
+});
+
