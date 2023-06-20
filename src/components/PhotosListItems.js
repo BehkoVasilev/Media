d4504de3ea0a9694f5280357ed69f7ccb0bffc5d
BehkoVasilev <vencislavvsilev@gmail.com>
2023-06-20 08:41:55 +0300

    arrangement of photos

--- src/components/PhotosListItems.js
+++ src/components/PhotosListItems.js
@@ -1,5 +1,5 @@
 function PhotosListItem({ photo }) {
-    return <div>
+    return <div className="relative m-2">
         <img className="h-20 w-20" src={photo.url} alt="random pic" />
     </div>
 };
